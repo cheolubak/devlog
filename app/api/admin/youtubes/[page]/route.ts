6207358f4746e2c37b497129277d5851d7ee9dbd
cheolubak <dev.cheolubak@gmail.com>
2026-02-22 21:30:12 +0900

    fix: youtube display toggle

--- app/api/admin/youtubes/[page]/route.ts
+++ app/api/admin/youtubes/[page]/route.ts
@@ -5,13 +5,17 @@ import { externalApi } from '@devlog/request';
 import { NextResponse } from 'next/server';
 
 export async function GET(
-  _: NextRequest,
+  req: NextRequest,
   { params }: { params: Promise<{ page: string }> },
 ) {
   const { page } = await params;
+  const { searchParams } = req.nextUrl;
+
+  const isDisplay = searchParams.get('isDisplay') ?? 'false';
 
   const res = await externalApi.get<ResponseList<PostListAll>>('posts/all', {
     params: {
+      isDisplay,
       offset: page,
       type: ['YOUTUBE'],
     },
