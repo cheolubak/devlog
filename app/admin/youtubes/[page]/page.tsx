4b1f2ad82468fb3a767557836dc156729a05d05c
cheolubak <dev.cheolubak@gmail.com>
2026-02-24 15:59:54 +0900

    fix: admin youtube isDisplay

--- app/admin/youtubes/[page]/page.tsx
+++ app/admin/youtubes/[page]/page.tsx
@@ -9,16 +9,22 @@ export const dynamic = 'force-dynamic';
 
 export default async function AdminPage({
   params,
+  searchParams,
 }: {
   params: Promise<{ page: number }>;
+  searchParams: Promise<{ isDisplay?: 'false' | 'true' }>;
 }) {
   const { page } = await params;
+  const { isDisplay = 'false' } = await searchParams;
 
   const res = await fetchApi.get<ResponseList<PostListAll>>(
     `admin/youtubes/${page}`,
     {
       next: {
         revalidate: 0,
+      },
+      params: {
+        isDisplay,
       },
     },
   );
